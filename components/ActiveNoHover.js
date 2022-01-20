fb9017f813f42ac922b447f48d73652ac05b8c65
Antonio Zamora <zamora88875@gmail.com>
2022-01-20 15:48:54 -0500

    Progress towards tab responsitivity

--- components/ActiveNoHover.js
+++ components/ActiveNoHover.js
@@ -7,6 +7,7 @@ export default function ActiveNoHover({
 	id,
 	status,
 	grabTabExistence,
+	disable,
 }) {
 	// Create a function that uses a global handler to say: when this element is moused over, change the state of hover from 0 to 1,
 	// and when it mouses out set the 1 to 0
@@ -82,32 +83,78 @@ export default function ActiveNoHover({
 				onMouseEnter={startHover}
 				onMouseLeave={endHover}
 				xmlns="http://www.w3.org/2000/svg"
-				width="13"
-				height="16.027"
-				viewBox="0 0 16.027 16.027"
+				width="24"
+				height="24"
+				viewBox="0 0 30 30"
 			>
-				<g id="Group_1" data-name="Group 1" transform="translate(-555.95 -27.925)">
-					<path
-						id="Path_19"
-						data-name="Path 19"
-						d="M550.6,32.632l14.613,14.613"
-						transform="translate(524.025 593.845) rotate(-90)"
-						fill="none"
-						stroke="#fff"
-						strokeWidth="2"
-					/>
-					<path
-						id="Path_20"
-						data-name="Path 20"
-						d="M550.6,32.632l14.613,14.613"
-						transform="translate(6.058 -4)"
-						fill="none"
-						stroke="#fff"
-						strokeWidth="2"
-					/>
-				</g>
+				<defs>
+					<linearGradient
+						id="linear-gradient"
+						x2="30"
+						y1="15"
+						y2="15"
+						gradientUnits="userSpaceOnUse"
+					>
+						<stop offset="0" stopOpacity="0"></stop>
+						<stop offset="0.04" stopOpacity="0.27"></stop>
+						<stop offset="0.32"></stop>
+						<stop offset="0.41"></stop>
+						<stop offset="1"></stop>
+					</linearGradient>
+				</defs>
+				<rect width="30" height="30" fill="red " data-name="Rectangle 21" rx="6"></rect>
+				<path
+					fill="none"
+					stroke="#fff"
+					strokeWidth="2"
+					d="M7.69 22.31L22.31 7.69"
+					data-name="Path 19"
+				></path>
+				<path
+					fill="none"
+					stroke="#fff"
+					strokeWidth="2"
+					d="M7.69 7.69l14.62 14.62"
+					data-name="Path 20"
+				></path>
 			</svg>
 		)
+
+		// (
+		// 	<svg
+		// className="tabCloseButton"
+		// style={positioning}
+		// onClick={destroy}
+		// data-tab={id}
+		// onMouseEnter={startHover}
+		// onMouseLeave={endHover}
+		// xmlns="http://www.w3.org/2000/svg"
+		// width="13"
+		// height="16.027"
+		// viewBox="0 0 16.027 16.027"
+		// 	>
+		// <g id="Group_1" data-name="Group 1" transform="translate(-555.95 -27.925)">
+		// 	<path
+		// 		id="Path_19"
+		// 		data-name="Path 19"
+		// 		d="M550.6,32.632l14.613,14.613"
+		// 		transform="translate(524.025 593.845) rotate(-90)"
+		// 		fill="none"
+		// 		stroke="#fff"
+		// 		strokeWidth="2"
+		// 	/>
+		// 	<path
+		// 		id="Path_20"
+		// 		data-name="Path 20"
+		// 		d="M550.6,32.632l14.613,14.613"
+		// 		transform="translate(6.058 -4)"
+		// 		fill="none"
+		// 		stroke="#fff"
+		// 		strokeWidth="2"
+		// 	/>
+		// </g>
+		// 	</svg>
+		// )
 	} else if (hover) {
 		output = (
 			<svg
@@ -153,7 +200,7 @@ export default function ActiveNoHover({
 				</g>
 			</svg>
 		)
-	} else {
+	} else if (disable) {
 		output = (
 			<svg
 				className="tabCloseButton"
